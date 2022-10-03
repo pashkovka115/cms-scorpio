608f6ecf7008f0f8b89c1ed612f3ab66fe9ffc0f
Sergey <s.smirnov@ylab.io>
2022-10-03 11:39:47 +0300

    Add all(), find()

--- Crm/Models/Model.php
+++ Crm/Models/Model.php
@@ -3,13 +3,28 @@
 namespace Crm\Models;
 
 
+use Crm\Builder\Query\Query;
+
 class Model
 {
-    public $table;
+    public static $table;
 
 
-    protected function all()
+    public static function find($id, $field = 'id', $mode = \PDO::FETCH_ASSOC)
     {
-//        return
+        if (!is_array($id)){
+            $id = [$id];
+        }
+        $res = Query::select(static::$table)->whereIN($field, $id)->query();
+
+        return $res->fetchAll($mode);
+    }
+
+
+    public static function all(array $params = [], $mode = \PDO::FETCH_ASSOC)
+    {
+        $res = Query::select(static::$table)->query($params);
+
+        return $res->fetchAll($mode);
     }
 }
