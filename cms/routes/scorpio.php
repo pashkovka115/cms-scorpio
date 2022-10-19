c8e9104353dc9430d32df91e77cf85e4cc7fddf3
Sergey <s.smirnov@ylab.io>
2022-10-19 08:46:57 +0300

    Добавил комментарии

--- cms/routes/scorpio.php
+++ cms/routes/scorpio.php
@@ -3,5 +3,6 @@
 /** @var Crm\Route\Route $route */
 
 $route->namespace('scorpio', function () use ($route) {
+    /** @namespace users */
     \App\Modules\Scorpio\Admin\Users\Index::getRoutes($route, \App\Admin\Controllers\UsersController::class);
 });
